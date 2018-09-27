5b7bf5f30400154f9f90b162cb0c51904b268fd8
Christopher Spring <chris@nolaspring.net>
2018-09-27 13:15:12 -0500

    added comms dev

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Dashboard.css';
 
-
 class Dashboard extends React.Component {
 
   state =  {
@@ -10,21 +9,25 @@ class Dashboard extends React.Component {
 
  componentDidMount() {
 
-  fetch('/api/all-devices')
-  .then( (response)=>{
+  fetch('/devices/all-devices')
+  .then( response =>{
     if (!response.ok) {
-    throw Error(response.statusText);
+      throw Error( {"message":"Unexpected response from server","data":response} );
     }
-    return response;
+    return response
    })
   .then(
     res => res.json())
   .then( (response) => {
-    var devices = Array.from(new Map(JSON.parse(response.data)).values());
-    this.setState({devices: devices});
+     if (!response.data) {
+       throw Error({"message":"did not find data in response object","data":response});
+     }
+      var devices = Array.from(new Map(JSON.parse(response.data)).values());
+      this.setState({devices: devices});
   })
   .catch( (error)=> {
-    console.log(error);
+    console.log("Could not load device data" + error.message);
+    this.setState( {devices : {}, error: "Could not communicate with backend" } );
   })
 
 
@@ -36,6 +39,7 @@ render() {
   return (
     <div>
       nolab.io device-status
+      
     </div>
   )
 }
