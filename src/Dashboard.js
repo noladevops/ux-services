89f10280040bda52170c7d38af0c72d2a0aa2f5f
Christopher Spring <chris@nolaspring.net>
2018-10-02 11:23:28 -0500

    messing with error display" "

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -32,7 +32,7 @@ class Dashboard extends React.Component {
       this.setState({"devices": devices});
   })
   .catch( (error)=> {
-    console.log("Could not load device data:\n " + JSON.stringify(error));
+    console.log("Could not load device data:\n " + error);
   //  this.setState( { error: "Could not communicate with backend" } );
   })
 
