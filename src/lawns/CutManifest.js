fa067f815f1db569e36a9e5016bddf50f1029a95
Christopher Spring <chris@nolaspring.net>
2018-11-02 17:32:44 -0500

    binds to filtered address -all adresses by default

--- src/lawns/CutManifest.js
+++ src/lawns/CutManifest.js
@@ -14,6 +14,9 @@ class CutManifest extends React.Component {
   constructor(props,context) {
       super(props,context);
       this.toggleDropdown = this.toggleDropdown.bind(this);
+
+      this.toggleDayDropdown = this.toggleDayDropdown.bind(this);
+      this.selectDay = this.selectDay.bind(this);
       this.selectCrewLead = this.selectCrewLead.bind(this);
       this.addressFilterChange = this.addressFilterChange.bind(this);
       this.customerFilterChange = this.customerFilterChange.bind(this);
@@ -21,8 +24,10 @@ class CutManifest extends React.Component {
 
   state = {
       dropdownOpen: false,
+      dayDropdownOpen: false,
       crewLeads: [],
       addresses: [],
+      selectedDay:"",
       addressFilter: "",
       customerFilter: "",
       filterEchoText: "",
@@ -63,7 +68,7 @@ class CutManifest extends React.Component {
           address.end = new Date(moment().day(address.day));
           return address;
         })
-          this.setState({addresses: timedAddresses,activeAddresses: timedAddresses, filterEchoText: "loaded " + timedAddresses.length + " addresses"});
+          this.setState({addresses: timedAddresses,activeAddresses: timedAddresses,filteredAddresses:timedAddresses, filterEchoText: "loaded " + timedAddresses.length + " addresses"});
       });
       fetch("/api/customers", {credentials: "include" })
       .then( response => {
@@ -99,12 +104,25 @@ class CutManifest extends React.Component {
     }));
   }
 
+  toggleDayDropdown() {
+    this.setState(prevState => ({
+      dayDropdownOpen: !prevState.dayDropdownOpen
+    }));
+  }
+
   selectCrewLead(event) {
 
     var crewLead = lodash.filter(this.state.crewLeads, x => x.name === event.target.innerText)[0];
     this.setState( {selectedCrewLead:  crewLead });
-    var crewAddresses = lodash.filter(this.state.addresses, x => x.lead === crewLead._id);
+    var crewAddresses = lodash.filter(this.state.activeAddresses, x => x.lead === crewLead._id);
     this.setState( {customerFilter: "", addressFilter:"",activeAddresses: crewAddresses,filterEchoText: event.target.innerText + " - " + crewAddresses.length + " addresses"});
+  }
+
+  selectDay(event){
+    var day = lodash.filter(this.state.days, x => x.name === event.target.innerText)[0];
+    this.setState( {selectedDay:  day });
+    var dayAddresses = lodash.filter(this.state.activeAddresses, x => x.cutDay === day._id);
+    this.setState( {customerFilter: "", addressFilter:"",activeAddresses: dayAddresses,filteredAddresses: dayAddresses,filterEchoText: event.target.innerText + " - " + dayAddresses.length + " addresses"});
   }
 
   addressFilterChange(event) {
@@ -124,6 +142,8 @@ class CutManifest extends React.Component {
     this.setState({customerFilter: event.target.value,addressFilter:"",filteredAddresses: this.getAddresses(mergedIds),filteredCustomers: activeCustomers, filterEchoText: event.target.value + " - " + activeCustomers.length + " customers" });
   }
 
+
+
   getAddresses(idArray){
     // console.log(this.state.activeAddresses);
     var addressArrays = idArray.map( id=>{
@@ -137,6 +157,11 @@ class CutManifest extends React.Component {
     let dropdownItems = this.state.crewLeads.map( (crewLead)=>{
       if (crewLead.name !== 'crew'){
         return <DropdownItem onClick={this.selectCrewLead} key={crewLead._id}>{crewLead.name}</DropdownItem>
+      } else { return}
+    })
+    let dayDropdownItems = this.state.days.map( (day)=>{
+      if (day.name !== 'crew'){
+        return <DropdownItem onClick={this.selectDay} key={day._id}>{day.name}</DropdownItem>
       } else { return}
     })
     return(
@@ -155,8 +180,18 @@ class CutManifest extends React.Component {
                   </Dropdown>
                 </Col>
                 <Col>
+                <Dropdown size="sm"  isOpen={this.state.dayDropdownOpen} toggle={this.toggleDayDropdown} >
+                  <DropdownToggle caret>
+                    Cut Day
+                  </DropdownToggle>
+                  <DropdownMenu left>
+                    {dayDropdownItems}
+                  </DropdownMenu>
+                </Dropdown>
+                </Col>
+                <Col>
                   <Alert color="primary">
-                      {this.state.selectedCrewLead.name}
+                      {this.state.selectedCrewLead.name + " " + this.state.selectedDay.name}
                   </Alert>
                 </Col>
                 <Col>
